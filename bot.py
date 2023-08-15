8ef7d32ed4278767b9ce42c24ea10540800f8541
mudkipz <mudkipz@example.com>
2023-08-15 14:37:44 +0000

    a

---
+++ bot.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TOKEN_KEY = os.getenv('TOKEN_KEY')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+@bot.event
+async def on_ready():
+    print("Logged in")
+
+@bot.command() 
+async def test(ctx, arg):
+    await ctx.send(arg)
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith("Hello"):
+        await message.channel.send("Hi")
+
+    await bot.process_commands(message)
+bot.run(TOKEN_KEY)
